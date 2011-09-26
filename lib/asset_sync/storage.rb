f1138454c92ecdb8f090b4322a8e2ed28830966f
David Rice <me@davidjrice.co.uk>
2011-09-26 19:23:58 +0100

    Bugfix

--- lib/asset_sync/storage.rb
+++ lib/asset_sync/storage.rb
@@ -30,7 +30,7 @@ module AssetSync
     end
 
     def get_remote_files
-      raise BucketNotFound.new("AWS Bucket: #{self.config.aws_bucket} not found.")
+      raise BucketNotFound.new("AWS Bucket: #{self.config.aws_bucket} not found.") unless bucket
       return bucket.files.map { |f| f.key }
     end
 
