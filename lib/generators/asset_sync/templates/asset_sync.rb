cff674015d901d9c6a3196c230f199ad487d3d4d
David Rice <me@davidjrice.co.uk>
2011-10-25 01:12:28 +0100

    Improve generators to generate AWS or Rackspace compatible initializer or yml

--- lib/generators/asset_sync/templates/asset_sync.rb
+++ lib/generators/asset_sync/templates/asset_sync.rb
@@ -1,7 +1,14 @@
 AssetSync.configure do |config|
-  config.aws_access_key = ENV['AWS_ACCESS_KEY_ID']
-  config.aws_access_secret = ENV['AWS_SECRET_ACCESS_KEY']
-  config.aws_bucket = ENV['FOG_DIRECTORY']
+  <%- if aws? -%>
+  config.fog_provider = 'AWS'
+  config.aws_access_key_id = ENV['AWS_ACCESS_KEY_ID']
+  config.aws_secret_access_key = ENV['AWS_SECRET_ACCESS_KEY']
+  <%- elsif rackspace? -%>
+  config.fog_provider = 'Rackspace'
+  config.rackspace_username = ENV['RACKSPACE_USERNAME']
+  config.rackspace_api_key = ENV['RACKSPACE_API_KEY']
+  <%- end -%>  
+  config.fog_directory = ENV['FOG_DIRECTORY']
   # config.fog_region = "eu-west-1"
   config.existing_remote_files = "keep"
 end
