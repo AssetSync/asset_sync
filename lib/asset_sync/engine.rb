6eaa70eb96c1be525eb8fe2c43cc73c870b1c5b5
Chad Boyd <hoverlover@gmail.com>
2012-01-28 17:34:00 -0600

    Now correctly setting config.fog_region.

--- lib/asset_sync/engine.rb
+++ lib/asset_sync/engine.rb
@@ -14,7 +14,7 @@ class Engine < Rails::Engine
       AssetSync.configure do |config|
         config.fog_provider = ENV['FOG_PROVIDER']
         config.fog_directory = ENV['FOG_DIRECTORY']
-        config.fog_directory = ENV['FOG_REGION']
+        config.fog_region = ENV['FOG_REGION']
 
         config.aws_access_key_id = ENV['AWS_ACCESS_KEY_ID']
         config.aws_secret_access_key = ENV['AWS_SECRET_ACCESS_KEY']
