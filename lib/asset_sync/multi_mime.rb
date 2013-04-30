d441b19c29d34a4ae70c839c656727debc1d9848
Sergey Zelvenskiy <sergey@actions.im>
2013-04-30 14:40:25 -0700

    changed mime resolution to always use mime-types

--- lib/asset_sync/multi_mime.rb
+++ lib/asset_sync/multi_mime.rb
@@ -1,19 +1,16 @@
+require 'mime/types'
+
 module AssetSync
   class MultiMime
 
-    types={
-      "ttf"=> "application/x-font-woff"
-    }
-
     def self.lookup(ext)
-
+      "#{MIME::Types.type_for(ext).first}"
       #if defined?(Rack::Mime)
       #  ext_with_dot = ".#{ext}"
       #  Rack::Mime.mime_type(ext_with_dot)
       #else
-      "#{MIME::Types.type_for(ext).first}"
+      #
       #end
-
     end
 
   end
