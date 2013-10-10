5ee345a7cd8b805a3f6b79c1b450254a6c208a6a
Frederik Vollert <frederik.vollert@dynport.de>
2013-10-10 19:07:12 +0200

    adds fog endpoint configuration parameter

--- lib/asset_sync/config.rb
+++ lib/asset_sync/config.rb
@@ -27,6 +27,7 @@ module AssetSync
     attr_accessor :fog_connection_options
     attr_accessor :fog_scheme
     attr_accessor :fog_port
+    attr_accessor :fog_enpoint
 
     # Amazon AWS
     attr_accessor :aws_access_key_id, :aws_secret_access_key, :aws_reduced_redundancy
@@ -177,6 +178,7 @@ module AssetSync
       options = { :provider => fog_provider }
       options.merge!({ scheme: fog_scheme }) if fog_scheme.present?
       options.merge!({ port: fog_port }) if fog_port.present?
+      options.merge!({ endpoint: endpoint }) if endpoint.present?
       options.merge!({ connection_options: fog_connection_options }) if fog_connection_options.present?
 
       if aws?
