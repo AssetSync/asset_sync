f3ddb4ea17f0411ae84f2d9dc8b710b36ba554d3
PikachuEXE <pikachuexe@gmail.com>
2017-05-19 16:10:30 +0800

    * Try to use mime-types 3.x API first

--- lib/asset_sync/multi_mime.rb
+++ lib/asset_sync/multi_mime.rb
@@ -5,13 +5,15 @@ module AssetSync
 
     def self.lookup(ext)
 
-      if defined?(Mime::Type)
-        Mime::Type.lookup_by_extension(ext)
-      elsif defined?(Rack::Mime)
-        ext_with_dot = ".#{ext}"
-        Rack::Mime.mime_type(ext_with_dot)
-      else
+      if defined?(::MIME::Types)
         ::MIME::Types.type_for(ext).first
+      elsif defined?(::Mime::Type)
+        ::Mime::Type.lookup_by_extension(ext)
+      elsif defined?(::Rack::Mime)
+        ext_with_dot = ".#{ext}"
+        ::Rack::Mime.mime_type(ext_with_dot)
+      else
+        raise "No library found for mime type lookup"
       end
 
     end
